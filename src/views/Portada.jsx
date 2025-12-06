7be38472f8f2f3a62137995f5b153ceee4520a6c
AixaFilsinger <macafil99@gmail.com>
2025-12-06 09:52:38 -0300

    s

--- src/views/Portada.jsx
+++ src/views/Portada.jsx
@@ -3,11 +3,19 @@ const Portada = () => {
     return (
         <section className="portada img-fluid">
              <h1 className="enfoque">
-        {"Maria Zoe".split("").map((letra, i) => (
-          <span key={i} style={{ animationDelay: `${i * 0.1}s` }}>
-            {letra}
-          </span>
-        ))}
+        {"Maria Zoe".split("").map((letra, i) =>{
+                    
+                    // Lógica para manejar el espacio:
+                    const contenido = letra === ' ' ? '\u00A0' : letra; 
+                    // '\u00A0' es el equivalente Unicode del &nbsp; (espacio de no separación)
+                    // Usamos esto para que el espacio no se colapse.
+                    
+                    return (
+                        <span key={i} style={{ animationDelay: `${i * 0.1}s` }}>
+                            {contenido}
+                        </span>
+                    );
+                })}
       </h1>
             <aside className="linea">
         <h2 className="enfoque mis">
